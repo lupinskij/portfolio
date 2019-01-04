2e88db121a7380b658cd942e8209ab831d1e8ffa
lupinskij <lupinskij@gmail.com>
2019-01-04 09:08:49 -0700

    add page load stagger

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,15 @@
 import React from 'react'
+import Stagger from 'react-stagger'
 
 import { Layout, Hero, Image, SEO } from 'components'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`jeff lupinski`, `design`, `product`]} />
-    <Hero title="I design thoughtful digital experiences with beautiful brand aesthetics." />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-  </Layout>
+const IndexPage = ({ isReady }) => (
+  <Stagger in={isReady}>
+    <Layout>
+      <SEO title="Home" keywords={[`jeff lupinski`, `design`, `product`]} />
+      <Hero title="I design thoughtful digital experiences with beautiful brand aesthetics." />
+    </Layout>
+  </Stagger>
 )
 
 export default IndexPage
