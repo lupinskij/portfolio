d07e733adf3218ff36e425da549f9eaf3eaeaf56
lupinskij <lupinskij@gmail.com>
2019-01-15 13:34:00 -0700

    Update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
-import { Layout, SEO } from 'components'
+import { Layout, SEO, Hero } from 'components'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Hero title="You hit a page that doesn&#39;t exist... the sadness." />
   </Layout>
 )
 
