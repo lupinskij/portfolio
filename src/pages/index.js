07591f9e0bdaceb70eb9e36819c635c57ed7c97b
lupinskij <lupinskij@gmail.com>
2019-01-29 09:24:26 -0700

    Remove Image import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Stagger from 'react-stagger'
 
-import { Layout, Hero, Image, SEO } from 'components'
+import { Layout, Hero, SEO } from 'components'
 
 const IndexPage = ({ isReady }) => (
   <Stagger in={isReady}>
     <Layout>
       <SEO title="Home" keywords={[`jeff lupinski`, `design`, `product`]} />
-      <Hero title="I design thoughtful digital experiences with beautiful brand aesthetics." />
+      <Hero title="Designer crafting digital experiences with thoughtful brand aesthetics." />
     </Layout>
   </Stagger>
 )
