723c845887d8de3d580928218db8435922231845
lupinskij <lupinskij@gmail.com>
2019-03-29 15:06:41 -0600

    AMC Project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Layout, Hero, SEO, UnderArmour } from 'components'
+import { Layout, Hero, SEO, UnderArmour, AMC } from 'components'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`jeff lupinski`, `design`, `product`]} />
     <Hero title="Crafting thoughtful digital experiences with delightful brand aesthetics." />
     <UnderArmour />
+    <AMC />
   </Layout>
 )
 
