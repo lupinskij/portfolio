30decda4eba67da7ca974c58cbbb3768026c7c6e
lupinskij <lupinskij@gmail.com>
2019-07-08 10:20:48 -0600

    Copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ const IndexPage = () => (
       title="Jeff Lupinski — Product Designer"
       keywords={[`jeff lupinski`, `design`, `product`]}
     />
-    <Hero title="Crafting thoughtful digital experiences through visual &amp; coded solutions." />
+    <Hero title="Crafting thoughtful digital experiences through visual &amp; interactive solutions." />
     <NavController>
       <ScrollSection>
         <UnderArmour />
