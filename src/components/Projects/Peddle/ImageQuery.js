bc1590e326b2a66cc08a2fdaf02ed89682b5f196
Jeff <iam@jefflupinski.com>
2022-04-29 12:33:27 -0600

    peddle

---
+++ src/components/Projects/Peddle/ImageQuery.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql, StaticQuery } from 'gatsby'
+
+function ImageQuery({ children }) {
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          imageOne: file(relativePath: { eq: "peddle-hero.png" }) {
+            childImageSharp {
+              fluid(maxWidth: 2000, quality: 90) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+          imageTwo: file(relativePath: { eq: "peddle-screens.png" }) {
+            childImageSharp {
+              fluid(maxWidth: 2000, quality: 90) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+          imageThree: file(relativePath: { eq: "peddle-web.png" }) {
+            childImageSharp {
+              fluid(maxWidth: 2000, quality: 90) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+          imageFour: file(relativePath: { eq: "peddle-mobile.png" }) {
+            childImageSharp {
+              fluid(maxWidth: 2000, quality: 90) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+          imageFive: file(relativePath: { eq: "peddle-mock.jpg" }) {
+            childImageSharp {
+              fluid(maxWidth: 2000, quality: 90) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+          imageSix: file(relativePath: { eq: "peddle-library.png" }) {
+            childImageSharp {
+              fluid(maxWidth: 2000, quality: 91) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+          imageSeven: file(relativePath: { eq: "peddle-system.png" }) {
+            childImageSharp {
+              fluid(maxWidth: 2000, quality: 90) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+      `}
+      render={data => children(data)}
+    />
+  )
+}
+
+ImageQuery.propTypes = {
+  children: PropTypes.func.isRequired,
+}
+
+ImageQuery.defaultProps = {}
+
+export default ImageQuery
